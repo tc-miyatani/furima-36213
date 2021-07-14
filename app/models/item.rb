b6b615ca7c91142ac64e82106490618c064ffbd5
tc-miyatani <miyahei38+tc@gmail.com>
2021-07-14 16:27:30 +0900

    itemモデルのアソシエーション・バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,41 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
+  belongs_to :user
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+
+    validates :price, format: {
+      with: /\A[0-9]+\z/,
+      message: 'is invalid. Input half-width characters'
+    }, numericality: {
+      only_integer: true,
+      greater_than: 300,
+      less_than: 9999999,
+      message: 'is out of setting range'
+    }
+
+    with_options numericality: {
+      other_than: 0,
+      message: "can't be blank"
+    } do
+      validates :category_id
+      validates :sales_status_id
+      validates :shipping_fee_status_id
+      validates :prefecture_id
+      validates :scheduled_delivery_id
+    end
+
+    validates :user
+  end
 end
