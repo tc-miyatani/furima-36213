b61320c02884d93e1353ac1c69630969aa0c87fa
tc-miyatani <miyahei38+tc@gmail.com>
2021-07-14 16:29:06 +0900

    出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,22 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+
+  def item_params
+    params.require(:item)
+      .permit(
+        :image, :name, :info,
+        :category_id, :sales_status_id, :prefecture_id,
+        :shipping_fee_status_id, :scheduled_delivery_id, :price
+      ).merge(user_id: current_user.id)
+  end
 end
