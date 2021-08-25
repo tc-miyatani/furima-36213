6deb03baaca35fc591ca39d1a4fdb90d2a516ffe
tc-miyatani <miyahei38+tc@gmail.com>
2021-08-26 08:57:49 +0900

    テスト用アカウント生成用seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# テスト用アカウントの生成
+# 購入者用
+User.create(
+  email: 'buy@com',
+  password: 'abc123', password_confirmation: 'abc123',
+  nickname: 'さとう',
+  last_name: '佐藤', first_name: '一郎',
+  last_name_kana: 'サトウ', first_name_kana: 'イチロウ',
+  birth_date: '2000-01-01',
+)
+# 出品者用
+User.create(
+  email: 'sell@com',
+  password: 'abc123', password_confirmation: 'abc123',
+  nickname: 'すずき',
+  last_name: '鈴木', first_name: '次郎',
+  last_name_kana: 'スズキ', first_name_kana: 'ジロウ',
+  birth_date: '2000-01-01',
+)
